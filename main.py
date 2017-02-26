9911ce39ab58d6e599f2a31eb9e6d811a95c29dc
Heather Cunningham <kcredwolf@gmail.com>
2017-02-25 21:30:53 -0600

    Updated template with styles and added functionality to main.py

--- main.py
+++ main.py
@@ -34,11 +34,18 @@ class Handler(webapp2.RequestHandler):
         self.write(self.render_str(template, **kw))
 
 class Art(db.Model):
-    pass
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render('front.html', title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
+
+
+        self.render('front.html', title=title, art=art, error=error, arts = arts)
+        
 
     def get(self):
         self.render_front()
@@ -48,7 +55,10 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("thanks!")
+            a = Art(title =title, art = art)
+            a.put()
+            self.redirect("/")
+
         else:
             error = "we need both a title and some artwork!"
             self.render_front(title=title, art=art, error = error)
